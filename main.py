94523638781ab83e336eac388fe7a42dbeacb9f7
YYaFFF <protsievskii022@gmail.com>
2025-06-08 21:16:20 +0300

    new

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import requests
 
 
-def get_weather_by_place():
-    places_we_need = ["Череповец", "Москва", "Лондон", "svo"]
-    for place in places_we_need:
-        url = url_content.format(place)
-        response = requests.get(url)
-        response.raise_for_status()
-        print(response.text)
+def fetch_weather(location, payload):
+    response = requests.get(url=f"https://wttr.in/{location}", params=payload)
+    response.raise_for_status()
+    return response.text
+
+
+def display_weather(weather_data):
+    print(weather_data)
+
+
+def main():
+    payload = {
+        "nq": "",
+        "M": "",
+        "T": "",
+        "lang": "ru",
+    }
+    locations = ["Череповец", "Москва", "Лондон", "svo"]
+    for loc in locations:
+        data = fetch_weather(loc, payload)
+        display_weather(data)
 
 
 if __name__ == "__main__":
-    url_content = "https://wttr.in/{}?nqMT&lang=ru"
-    get_weather_by_place()
+    main()
